76d46e1d0c5f59f4969883759c5e5512d6a5f1a9
Chobi <cobos.luque.a@gmail.com>
2025-05-13 13:14:52 +0200

    Refactor saturated_lines_searcher to correctly identify saturated peaks and improve candidate selection logic. Selects the value of the center of the line.

--- saturated_lines_searcher.py
+++ saturated_lines_searcher.py
@@ -14,9 +14,27 @@ def saturated_lines_searcher(X, Y):
 
     #These candidates are basically all negative slope positions
     #Add one since using 'valid' shrinks the arrays
-    s_candidates = np.where(Y > threshold)
+    s_candidates = np.where(Y > threshold)[0]
+
+    print("print s_candidates =", s_candidates)
+
+    def center_value(arr):
+        n = len(arr)
+        return arr[n // 2 - 1] if n % 2 == 0 else arr[n // 2]
+    
+    saturated_peaks_indices = []
+    store_cons_points = []
+
+    for idx, val in enumerate(s_candidates):
+        if idx == 0 or val == s_candidates[idx - 1] + 1:
+            store_cons_points.append(val)
+        else:
+            saturated_peaks_indices.append(center_value(store_cons_points))
+            store_cons_points = [val]
+    if store_cons_points:
+        saturated_peaks_indices.append(center_value(store_cons_points))
 
     #The X values corresponding to the saturated peaks
-    saturated_peaks = X[s_candidates]
+    saturated_peaks = X[saturated_peaks_indices]
 
     return saturated_peaks
