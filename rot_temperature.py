134e14a8aff55b100a6a406f63fd55933b202131
Chobi <redjokersf@gmail.com>
2025-05-27 16:19:32 +0200

    temperature implementation

---
+++ rot_temperature.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def rot_temperature_C2(x, y):
+
+
+    return temperature, error
+
+def rot_temperature_OH(x, y):
+
+
+
+    return temperature, error
+
+def rot_temperature_CN(x, y):
+
+
+
+    return temperature, error
